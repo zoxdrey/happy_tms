3d8b4a87329e339074f14be06c6cae19be62896e
Petrov Vyacheslav <zoxypva@gmail.com>
2021-03-04 21:24:56 +0300

    init project, add material styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header className="mdl-layout__header">
+      <div className="mdl-layout__header-row">
+      <span className="mdl-layout-title">Title</span>
+      <div className="mdl-layout-spacer"></div>
+      <nav className="mdl-navigation mdl-layout--large-screen-only">
+        <a className="mdl-navigation__link" href="/some/valid/uri">Link</a>
+        <a className="mdl-navigation__link" href="/some/valid/uri">Link</a>
+        <a className="mdl-navigation__link" href="/some/valid/uri">Link</a>
+        <a className="mdl-navigation__link" href="/some/valid/uri">Link</a>
+      </nav>
+    </div>
+  </header>
+    <main className="page-content">
+      <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+        Add row
+      </button>
+      <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+        Add column
+      </button>
+      </main>
     </div>
   );
 }
