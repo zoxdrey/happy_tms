395c2621b7fa2aebc2ea7a3553cbda48438916b9
Petrov Vyacheslav <zoxypva@gmail.com>
2021-03-16 00:14:26 +0300

    template

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use('/api/auth', require('./routes/auth.routes'))
+const PORT = config.get('port') || 5000;
+const MONGOURI = config.get('mongoUri') || "mongodb://localhost:27017/";
+
+async function start() {
+    try {
+      await mongoose.connect(MONGOURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      });
+    } catch(e) {
+        console.log('Server error', e.message);
+        process.exit(1);
+    }
+}
+
+start();
+
+app.listen(PORT, () => console.log(`server start ${PORT}`));
+
---
+++ routes/auth.routes.js
@@ -0,0 +1,26 @@
+const {Router} = require('express');
+const User = require('../models/User');
+const router = Router();
+
+router.post('/register', async (req, res) => {
+    try {
+
+        const {email, password} = req.body;
+
+        const candidate = await User.findOne({email})
+
+        if(candidate) {
+           return  res.status(400).json({message: "Пользователь существует"})
+        }
+
+        
+    } catch(e) {
+        res.status(500).json({message: "Что то пошло не так"});
+    }
+})
+
+router.post('/login', async (req, res) => {
+
+})
+
+module.exports = router;
